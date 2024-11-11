96fbbe0e8f5334f3c58c47caef7513f2afb2efb2
Kat Kipling <Kat.Kipling@proton.me>
2024-11-11 12:11:23 +0000

    Base setup for a Player class

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+public class Player
+{
+    private String name;
+    private int playOrder;
+
+    public Player(String name, int playOrder)
+    {
+        this.name = name;
+        this.playOrder = playOrder;
+    }
+}
