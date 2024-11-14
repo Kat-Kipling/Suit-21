d693bdcf83ae7b8b7c02c630b5115732c74d4211
Kat Kipling <kat.kipling@cirdan.com>
2024-11-14 10:55:54 +0000

    Removed unneeded code

--- src/Suit.java
+++ src/Suit.java
@@ -1,27 +1,7 @@
 public enum Suit
 {
-    CLUBS(1),
-    DIAMONDS(2),
-    HEARTS(3),
-    SPADES(4);
-
-    private final int value;
-
-    Suit(int value)
-    {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public static Suit fromValue(int value) {
-        for (Suit suit : Suit.values()) {
-            if (suit.getValue() == value) {
-                return suit;
-            }
-        }
-        throw new IllegalArgumentException("Invalid value: " + value);
-    }
+    CLUBS(),
+    DIAMONDS(),
+    HEARTS(),
+    SPADES();
 }
