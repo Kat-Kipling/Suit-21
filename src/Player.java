51f7b0f49fa131eae901dcc07aaeec2ccf844c39
Kat Kipling <kat.kipling@proton.me>
2024-12-16 15:33:05 +0000

    Added functions for scoring hand

--- src/Player.java
+++ src/Player.java
@@ -1,35 +1,38 @@
-import java.util.ArrayList;
-
-public class Player
-{
+public class Player {
     private String name;
     private int playerNumber;
     private Hand hand;
 
 
-    public Player(String name, int playOrder)
-    {
+    public Player(String name, int playOrder) {
         this.name = name;
         this.playerNumber = playOrder;
         this.hand = new Hand();
     }
 
-    public void addCard(Card card)
-    {
+    public void addCard(Card card) {
         this.hand.add(card);
     }
 
-    public void clearHand()
-    {
+    public void clearHand() {
         this.hand.clear();
     }
 
-    public String getHand()
-    {
+    public String getHand() {
         return this.hand.toString();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int scoreSuit(Suit suitToScore)
+    {
+        return this.hand.scoreSuit(suitToScore);
+    }
+
+    public int[] scoreHand()
+    {
+        return this.hand.scoreHand();
     }
 }
