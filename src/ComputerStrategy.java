dfb80eb9fb27cd0bae7b5be4530bf5e4366e20a3
Kat Kipling <kat.kipling@cirdan.com>
2024-12-19 14:49:43 +0000

    Implementation of AI with more logic

--- src/ComputerStrategy.java
+++ src/ComputerStrategy.java
@@ -1,11 +1,37 @@
 public class ComputerStrategy implements PlayerStrategy
 {
+    private ScoreCalculator scoreCalculator = new ScoreCalculator();
+
     @Override
-    public Card chooseCardToPlay(Player player, Hand hand)
-    {
-        player.displayHand();
-        // Simple AI: Play the first card
-        return player.getCard(0);
+    public Card chooseCardToPlay(Player player, Hand hand) {
+        Card cardToRemove = null;
+        int maxSuitScore = 0;
+        Suit targetSuit = null;
+
+        // Determine the target suit (closest to 21 without exceeding it)
+        for (Suit suit : Suit.values()) {
+            int suitScore = scoreCalculator.scoreSuit(suit, hand);
+            if (suitScore <= 21 && suitScore > maxSuitScore) {
+                maxSuitScore = suitScore;
+                targetSuit = suit;
+            }
+        }
+
+        // Find a card that is the least useful
+        for (Card card : hand.getHand()) {
+            if (!card.getSuit().equals(targetSuit)) {
+                cardToRemove = card;
+                break;
+            }
+        }
+
+        // If all cards are in the target suit, remove the lowest value card
+        if (cardToRemove == null && targetSuit != null)
+        {
+            cardToRemove = hand.getLowestValueCardInSuit(targetSuit);
+        }
+
+        return cardToRemove;
     }
 
     @Override
@@ -19,3 +45,5 @@ public class ComputerStrategy implements PlayerStrategy
         else return false;
     }
 }
+
+
