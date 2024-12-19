8c86df9030faffbe4faaea77340db2b46129162b
Kat Kipling <kat.kipling@cirdan.com>
2024-12-19 09:46:34 +0000

    Player logic moved to strategy

--- src/HumanStrategy.java
+++ src/HumanStrategy.java
@@ -1,3 +1,20 @@
-public class HumanStrategy implements PlayerStrategy;
+import java.util.Scanner;
+
+public class HumanStrategy implements PlayerStrategy
 {
+    private Scanner input;
+
+    public HumanStrategy(Scanner input)
+    {
+        this.input = input;
+    }
+
+    @Override
+    public Card chooseCardToPlay(Player player, Hand hand)
+    {
+        player.displayHand();
+        System.out.println("Choose a card to play (enter index 1-5):");
+        int index = input.nextInt() - 1;
+        return player.getCard(index);
+    }
 }
